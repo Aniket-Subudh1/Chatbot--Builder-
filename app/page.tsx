a2af0b0c9b66d7446e7b2eb8d889a3101e3a90f5
Basir Khan <khanbasir5555@gmail.com>
2024-09-03 15:48:38 +0530

    booking conf added

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,23 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
 import Hero from '@/utils/user/Hero'
 import Chatbot from '@/utils/user/Chatbot'
+import Booking from '@/utils/user/Booking'
+import { useSearchParams } from 'next/navigation'
 const page = () => {
+  const searchParams = useSearchParams()
+  const [showBooking,setShowBooking] = useState(false)
+  useEffect(()=>{
+  if(searchParams.get('id')!=null){
+    setShowBooking(true)
+  }
+  },[])
   return (
     <div>
-      <Hero/>
+      {!showBooking&&<Hero/>}
+      {showBooking&&<Booking/>}
       <div className='absolute bottom-0 right-0 '>
-        <Chatbot/>
+        {!showBooking&&<Chatbot/>}
         <script src="https://checkout.razorpay.com/v1/checkout.js"></script>
       </div>
     </div>
